ba6e67c18e71412dc211c4eff4e7476c51aeca84
TheDiamondYT1 <goldowl37@gmail.com>
2017-06-01 00:02:53 +0100

    Add more boilerplate code

---
+++ api/block.js
@@ -0,0 +1,45 @@
+/**
+ *
+ *   _____                  _     _____           _       _   
+ *  / ____|                | |   / ____|         (_)     | |  
+ *  | (___  _ __   __ _ _ __| | _| (___   ___ _ __ _ _ __ | |_ 
+ *  \___ \| '_ \ / _` | '__| |/ /\___ \ / __| '__| | '_ \| __|
+ *  ____) | |_) | (_| | |  |   < ____) | (__| |  | | |_) | |_ 
+ * |_____/| .__/ \__,_|_|  |_|\_\_____/ \___|_|  |_| .__/ \__|
+ *        | |                                      | |        
+ *        |_|                                      |_|        
+ *
+ *
+ * This file is part of SparkScript by SparkDevs. 
+ *
+ * You may look at the code, you may change the code. But you MUST give us credit.
+ * Please try and keep the API more or less the same so we can have a universal 
+ * modding API for Minecraft: Pocket Edition. Happy coding :)
+ *
+ * Website: https://sparkdevs.net/
+ * Twitter: https://twitter.com/SparkDevTeam/
+ *
+ * LICENSE: LGPL
+ */
+
+(function() {
+	/** @var array */
+	var blocks = [];
+
+	/**
+	 * Creates a new block.
+	 *
+	 * @return this
+	 */
+	create = function(data) {
+		// I can't figure out how to do this...im dumb lol
+ 		return this;
+	},
+
+	/**
+	 * Registers the block we created earlier.
+	 */
+	register = function() {
+		// TODO
+	}
+})(); 
---
+++ core/console.js
@@ -0,0 +1,33 @@
+/**
+ *
+ *   _____                  _     _____           _       _   
+ *  / ____|                | |   / ____|         (_)     | |  
+ *  | (___  _ __   __ _ _ __| | _| (___   ___ _ __ _ _ __ | |_ 
+ *  \___ \| '_ \ / _` | '__| |/ /\___ \ / __| '__| | '_ \| __|
+ *  ____) | |_) | (_| | |  |   < ____) | (__| |  | | |_) | |_ 
+ * |_____/| .__/ \__,_|_|  |_|\_\_____/ \___|_|  |_| .__/ \__|
+ *        | |                                      | |        
+ *        |_|                                      |_|        
+ *
+ *
+ * This file is part of SparkScript by SparkDevs. 
+ *
+ * You may look at the code, you may change the code. But you MUST give us credit.
+ * Please try and keep the API more or less the same so we can have a universal 
+ * modding API for Minecraft: Pocket Edition. Happy coding :)
+ *
+ * Website: https://sparkdevs.net/
+ * Twitter: https://twitter.com/SparkDevTeam/
+ *
+ * LICENSE: LGPL
+ */
+
+(function() {
+	log = function(text) {
+		_log(text);
+	},
+
+	warn = function(text) {
+		_log(text);
+	}
+})(); 
